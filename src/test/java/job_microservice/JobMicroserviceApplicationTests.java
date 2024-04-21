6b3384b4964a419bf87a3b0f1b159662acad4bd9
Fredrick Kabiru <mukii7385@gmail.com>
2024-04-21 10:09:41 +0300

    initialize

---
+++ src/test/java/job_microservice/JobMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package job_microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
